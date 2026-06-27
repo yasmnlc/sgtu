8e53dbd3bd23f9967dee16b446c65ef5fdd723ef
yasmnlc <lc.yasmn@gmail.com>
2026-06-27 17:14:48 -0300

    Feat: Adiciona funcionalidade de recuperação de senha e campo de e-mail no formulário de cadastro

--- backend/app/models/usuario.py
+++ backend/app/models/usuario.py
@@ -4,6 +4,7 @@ from typing import Optional
 class UsuarioCriar(BaseModel):
     nome_completo: str
     cpf: str
+    email: str
     senha: str
     perfil: str = "estudante"  # Pode ser: estudante, motorista, secretaria
 
@@ -15,6 +16,7 @@ class UsuarioLogin(BaseModel):
 class UsuarioNoBanco(BaseModel):
     nome_completo: str
     cpf: str
+    email: str
     senha_hash: str
     perfil: str
     primeiro_acesso: bool = False 
